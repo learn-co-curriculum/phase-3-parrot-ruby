e1532f9041a08eec32b1bf0f735fbbad2d32b382
Brian Sitati <briansit93@gmail.com>
2022-06-08 11:50:47 +0300

    commit tree 829eac337ed19b13e09a655b4f9c548ca8ea376c

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+# def parrot
+#   puts "Squawk!"
+# end
+
+
+def parrot (phrase="Pretty bird!")
+    puts ("Pretty bird!")
+end
+
