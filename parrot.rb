a9180ec7368eeef66e28d81916c9f48cf9e37dea
Cecilia Milla <cmilla1020@gmail.com>
2022-12-05 16:41:51 -0500

    commit tree 1ed432d4015ade65f1d196d3c2526ff1b1a7ad03

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot phrase="Squawk!"
+    puts phrase
+    return phrase
+end
+
+parrot "Polly wants a cracker! "
+parrot "Pretty bird"
